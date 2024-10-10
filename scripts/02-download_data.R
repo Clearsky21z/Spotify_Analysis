974a0266b49cd8582bdb5e974edfc5e624fd4280
JUNHAN ZHANG <144378024+Clearsky21z@users.noreply.github.com>
2024-10-10 16:59:55 -0400

    Download the data

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Spotify API
 # Author: Rohan Alexander [...UPDATE THIS...]
 # Date: 11 February 2023 [...UPDATE THIS...]
 # Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
@@ -9,18 +9,21 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
+library(spotifyr)
 # [...UPDATE THIS...]
 
 #### Download data ####
 # [...ADD CODE HERE TO DOWNLOAD...]
-
+radiohead <- get_artist_audio_features("radiohead")
+the_national <- get_artist_audio_features("the national")
+ed_sheeran <- get_artist_audio_features("ed sheeran")
+jj_lin <- get_artist_audio_features("jj lin")
 
 
 #### Save data ####
-# [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+saveRDS(radiohead, "data/01-raw_data/radiohead.rds")
+saveRDS(the_national, "data/01-raw_data/the_national.rds")
+saveRDS(ed_sheeran, "data/01-raw_data/ed_sheeran.rds")
+saveRDS(jj_lin, "data/01-raw_data/jj_lin.rds")
